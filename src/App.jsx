812126f8628f2f3e1a36cb68c22faf36e92195c9
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-08-31 09:57:35 -0300

    Aplicando class component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ class Equipe extends Component{
   render(){
 return(
   <div>
+    <Sobre nome={this.props.nome} cargo={this.props.cargo} idade={this.props.idade} />
+    <hr />
+  </div>
+)
+  }
+}
+
+class Sobre extends Component{
+  render(){
+    return(
+      <div>
     <h2>Olá sou o(a) {this.props.nome}</h2>
     <h3>Cargo: {this.props.cargo}</h3>
     <h3>idade: {this.props.idade} anos</h3>
   </div>
-)
+    )
   }
 }
 
@@ -17,6 +28,7 @@ function App() {
     <div>
       <h1>Conheça nossa equipe:</h1> 
      <Equipe nome= "Katherine" cargo="Duplicata" idade="567" />
+     <Equipe nome= "Emily" cargo="Bruxa" idade="27" />
     </div>
   );
 }
