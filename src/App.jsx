ad3a201c3e963a85be6516f7c977c65dd311c57d
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-10-01 12:11:11 -0300

    Treinando

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,17 @@ import React, { Component }from "react";
 
 class App extends Component{
 
-  constructor(props){
-    super(props);
-    this.state = {
-hora: '00:00:00'
-
-    };
-  };
-
-componentDidMount(){
-
-setInterval(()=>{
-  this.setState({ hora: new Date().toLocaleTimeString()})
-}, 1000);
+aumentar(){
+  let state = this.state;
+  state.contador +=1;
+  this.setState(state)
 
 }
-
-componentDidUpdate(){
-  console.log('Atualizou!')
-}
-
-
 
   render(){
   return(
     <div>
-     <h1>My project {this.state.hora}</h1>
+  
     </div>
   );
 }
