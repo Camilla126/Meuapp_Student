997b045eb482a68ae27ee16fbf2aa0152080bcab
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-10-14 11:58:13 -0300

    Cronometro finalizafo!

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ constructor(props){
   super(props);
   this.state = {
     numero: 0,
-    botao: 'VAI'
+    botao: 'INICIAR'
   };
 this.timer = null;
   this.vai = this.vai.bind(this);
@@ -21,7 +21,7 @@ let state = this.state;
   if(this.timer !== null){
     clearInterval(this.timer);
     this.timer = null;
-state.botao = 'VAI';
+state.botao = 'INICIAR';
   }else{
 this.timer = setInterval(()=>{
   let state = this.state;
@@ -35,7 +35,15 @@ state.botao = 'PAUSAR'
 
 
 limpar(){
+  if(this.timer !== null){
+    clearInterval(this.timer);
+    this.timer = null;
 
+  }
+  let state = this.state;
+  state.numero = 0;
+  state.botao = 'INICIAR';
+  this.setState(state);
 }
   render() {
     return (
